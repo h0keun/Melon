e9f92353c2109fb0564ad53a29f66d105073e324
h0keun <dlghrms95@gmail.com>
2021-05-28 16:29:37 +0900

    NCS_music crawling, make API & Retrofit2

--- app/src/main/java/com/melon/PlayerFragment.kt
+++ app/src/main/java/com/melon/PlayerFragment.kt
@@ -1,14 +1,49 @@
 package com.melon
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.melon.service.MusicDto
+import com.melon.service.MusicService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class PlayerFragment: Fragment(R.layout.fragment_player) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        getVideoListFromServer()
+    }
+
+    private fun getVideoListFromServer() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(MusicService::class.java)
+            .also {
+                it.listMusics()
+                    .enqueue(object : Callback<MusicDto>{
+                        override fun onResponse(
+                            call: Call<MusicDto>,
+                            response: Response<MusicDto>
+                        ) {
+                            Log.d("PlayerFragment", "${response.body()}")
+                        }
+
+                        override fun onFailure(call: Call<MusicDto>, t: Throwable) {
+
+                        }
+
+                    })
+            }
     }
 
     companion object { // newInstance 로 인자를 넘겨줄 때 apply 를 통해 arguments 를 쉽게 추가해줄 수 있다.
